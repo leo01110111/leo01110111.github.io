7cc9c33a45e58438b28cefc334389bb451beffb9
leo01110111 <84789689+leo01110111@users.noreply.github.com>
2026-04-10 16:17:17 +0000

    Deploying to gh-pages from @ leo01110111/leo01110111.github.io@d16098b72bf597e3ab2ce3524e09a21e9ef056f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,6 +78,11 @@ ninja.data = [{
           description: "What is the math involved in inverse kinematics for a 6-DoF robotic arm? What does it take to build one from scratch?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Robotic_Arm/";
+            },},{id: "projects-jepa-from-scratch",
+          title: 'JEPA from Scratch',
+          description: "My investigtions into models that solve the autoregressive problem.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/jepa_from_scratch/";
             },},{id: "projects-mechanistic-interpretability-for-vla-models",
           title: 'Mechanistic Interpretability for VLA models',
           description: "What can we learn about vision language action models from its weights?",
